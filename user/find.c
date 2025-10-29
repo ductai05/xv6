52d187db60ea04a37624ff7ba5fbb591fe1bdbd3
htrung1105 <23122004@student.hcmus.edu.vn>
2025-10-29 15:24:47 +0700

    fix: find & feat: basename
    Chỉ tìm kiếm các tệp, không tìm kiếm các thư mục trùng với tên filename

--- user/find.c
+++ user/find.c
@@ -4,65 +4,61 @@
 #include "kernel/fs.h"
 #include "kernel/fcntl.h"
 
-void find(char *path, const char *target_name)
+char* basename(char *path)
+{
+  char *p;
+
+  // Find first character after last slash
+  for(p=path+strlen(path); p >= path && *p != '/'; p--);
+  p++;
+
+  return p;
+}
+
+void find(char *path, const char *filename)
 {
   char buf[512], *p;
   int fd;
   struct dirent de;
   struct stat st;
 
-  if((fd = open(path, O_RDONLY)) < 0){
+  if((fd = open(path, O_RDONLY)) < 0) {
     fprintf(2, "find: cannot open %s\n", path);
     return;
   }
 
-  if(fstat(fd, &st) < 0){
+  if(fstat(fd, &st) < 0) {
     fprintf(2, "find: cannot stat %s\n", path);
     close(fd);
     return;
   }
 
-  if(st.type != T_DIR){
-    close(fd);
-    return;
+  switch(st.type) {
+    case T_FILE:
+    if(strcmp(basename(path), filename) == 0){
+        printf("%s\n", path);
+    }
+    break;
+
+    case T_DIR:
+    if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+      printf("find: path too long\n");
+      break;
+    }
+    strcpy(buf, path);
+    p = buf+strlen(buf);
+    *p++ = '/';
+    while(read(fd, &de, sizeof(de)) == sizeof(de)){
+      if(de.inum == 0)
+        continue;
+      memmove(p, de.name, DIRSIZ);
+      p[DIRSIZ] = 0;
+      if (strcmp(de.name, ".") != 0 && strcmp(de.name, "..") != 0) {
+          find(buf, filename);
+      }
+    }
+    break;
   }
-
-  if(strlen(path) + 1 + DIRSIZ + 1 > sizeof(buf)){
-    fprintf(2, "find: path too long\n");
-    close(fd);
-    return;
-  }
-  
-  strcpy(buf, path);
-  p = buf + strlen(buf);
-  *p++ = '/';
-
-  while(read(fd, &de, sizeof(de)) == sizeof(de)){
-    if(de.inum == 0)
-      continue;
-
-    if(strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0){
-      continue;
-    }
-    
-    memmove(p, de.name, DIRSIZ);
-    p[DIRSIZ] = 0;
-
-    if(strcmp(de.name, target_name) == 0){
-        printf("%s\n", buf);
-    }
-    
-    struct stat st2;
-    if(stat(buf, &st2) < 0){
-      fprintf(2, "find: cannot stat %s\n", buf);
-      continue;
-    }
-
-    if(st2.type == T_DIR){
-      find(buf, target_name);
-    }
-  }
-
   close(fd);
 }
 
